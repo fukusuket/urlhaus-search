14323be2c28c2ed0fbbb4b5bf7360d2606cdec4b
fukusuket <fukusuke.takahashi@gmail.com>
2022-08-16 06:40:46 +0900

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,21 @@ mod my_date_format {
 
     const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
 
-    pub fn serialize<S>(
-        date: &DateTime<Utc>,
-        serializer: S,
-    ) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
+    pub fn serialize<S>(date: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
     {
         let s = format!("{}", date.format(FORMAT));
         serializer.serialize_str(&s)
     }
 
-    pub fn deserialize<'de, D>(
-        deserializer: D,
-    ) -> Result<DateTime<Utc>, D::Error>
-        where
-            D: Deserializer<'de>,
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        Utc.datetime_from_str(&s[0..18], FORMAT).map_err(serde::de::Error::custom)
+        Utc.datetime_from_str(&s[0..18], FORMAT)
+            .map_err(serde::de::Error::custom)
     }
 }
 
@@ -86,7 +82,6 @@ struct ThreatFoxEntry {
     tags: Vec<String>,
 }
 
-
 #[derive(Parser, Debug)]
 #[clap(about, long_about = None)]
 struct Args {
@@ -129,21 +124,31 @@ struct Args {
 
 fn get_response2(args: &Args, client: Client) -> UrlhausResponse {
     let param = [("tag", args.tag.as_str())];
-    let res = client.post("https://urlhaus-api.abuse.ch/v1/tag").form(&param).send();
+    let res = client
+        .post("https://urlhaus-api.abuse.ch/v1/tag")
+        .form(&param)
+        .send();
     let res_data = match res {
         Ok(r) => r,
-        Err(e) => panic!("failed to get abuse.ch api response. [{:?}]", e)
+        Err(e) => panic!("failed to get abuse.ch api response. [{:?}]", e),
     };
     let res_json: UrlhausResponse = res_data.json().unwrap();
     res_json
 }
 
 fn get_response1(args: &Args, client: &Client) -> ThreatfoxResponse {
-    let param = HashMap::from([("query", "taginfo"), ("tag", args.tag.as_str()), ("limit", "1000")]);
-    let res = client.post("https://threatfox-api.abuse.ch/api/v1/").json(&param).send();
+    let param = HashMap::from([
+        ("query", "taginfo"),
+        ("tag", args.tag.as_str()),
+        ("limit", "1000"),
+    ]);
+    let res = client
+        .post("https://threatfox-api.abuse.ch/api/v1/")
+        .json(&param)
+        .send();
     let res_data = match res {
         Ok(r) => r,
-        Err(e) => panic!("failed to get abuse.ch api response. [{:?}]", e)
+        Err(e) => panic!("failed to get abuse.ch api response. [{:?}]", e),
     };
     let res_json: ThreatfoxResponse = res_data.json().unwrap();
     res_json
@@ -153,7 +158,8 @@ fn to_json<T: Serialize>(res_json: &[T]) {
     let content = serde_json::to_string_pretty(res_json).unwrap();
     let f = File::create("result.json").expect("Unable to create file.");
     let mut f = BufWriter::new(f);
-    f.write_all(content.as_bytes()).expect("Unable to write file.");
+    f.write_all(content.as_bytes())
+        .expect("Unable to write file.");
     println!("outputted. [{:?}].", f)
 }
 
@@ -168,52 +174,128 @@ fn main() {
     let client = reqwest::blocking::Client::new();
     if args.api.contains("threatfox") {
         let res_json = get_response1(&args, &client);
-        let res_entries: Vec<ThreatFoxEntry> = res_json.data.into_iter().
-            filter(|e|
-                e.reporter.contains(&args.reporter) &&
-                !e.ioc_type.contains(&args.exclude_ioc) &&
-                e.first_seen >= Utc.datetime_from_str(&format!("{}{}", &args.date_from, "000000"), "%Y%m%d%H%M%S").unwrap_or(Utc::now()) &&
-                e.first_seen <= Utc.datetime_from_str(&format!("{}{}", &args.date_to, "000000"), "%Y%m%d%H%M%S").unwrap_or(Utc::now()))
+        let res_entries: Vec<ThreatFoxEntry> = res_json
+            .data
+            .into_iter()
+            .filter(|e| {
+                e.reporter.contains(&args.reporter)
+                    && !e.ioc_type.contains(&args.exclude_ioc)
+                    && e.first_seen
+                        >= Utc
+                            .datetime_from_str(
+                                &format!("{}{}", &args.date_from, "000000"),
+                                "%Y%m%d%H%M%S",
+                            )
+                            .unwrap_or(Utc::now())
+                    && e.first_seen
+                        <= Utc
+                            .datetime_from_str(
+                                &format!("{}{}", &args.date_to, "000000"),
+                                "%Y%m%d%H%M%S",
+                            )
+                            .unwrap_or(Utc::now())
+            })
             .collect();
         match args.format {
             Some(x) if x.to_lowercase().eq("csv") => {
                 let f = File::create("result.csv").expect("Unable to create file.");
-                let mut wtr = WriterBuilder::new().quote_style(QuoteStyle::Always).from_writer(BufWriter::new(f));
-                let _ = wtr.write_record(&["id", "ioc", "threat_type", "threat_type_desc", "ioc_type", "ioc_type_desc", "malware", "malware_printable", "malware_alias", "malware_malpedia", "confidence_level", "first_seen", "reporter", "tags"]);
+                let mut wtr = WriterBuilder::new()
+                    .quote_style(QuoteStyle::Always)
+                    .from_writer(BufWriter::new(f));
+                let _ = wtr.write_record(&[
+                    "id",
+                    "ioc",
+                    "threat_type",
+                    "threat_type_desc",
+                    "ioc_type",
+                    "ioc_type_desc",
+                    "malware",
+                    "malware_printable",
+                    "malware_alias",
+                    "malware_malpedia",
+                    "confidence_level",
+                    "first_seen",
+                    "reporter",
+                    "tags",
+                ]);
                 for e in res_entries {
-                    let _ = wtr.write_record(&[&e.id, &e.ioc.replace("http", "hxxp"), &e.threat_type, &e.threat_type_desc, &e.ioc_type, &e.ioc_type_desc, &e.malware, &e.malware_printable, &e.malware_alias, &e.malware_malpedia, &e.confidence_level.to_string(), &e.first_seen.to_string(), &e.reporter, &e.tags.join(":")]);
+                    let _ = wtr.write_record(&[
+                        &e.id,
+                        &e.ioc.replace("http", "hxxp"),
+                        &e.threat_type,
+                        &e.threat_type_desc,
+                        &e.ioc_type,
+                        &e.ioc_type_desc,
+                        &e.malware,
+                        &e.malware_printable,
+                        &e.malware_alias,
+                        &e.malware_malpedia,
+                        &e.confidence_level.to_string(),
+                        &e.first_seen.to_string(),
+                        &e.reporter,
+                        &e.tags.join(":"),
+                    ]);
                 }
                 println!("outputted. [{}].", "result.csv");
-            },
+            }
             Some(x) if x.to_lowercase().eq("json") => to_json(&res_entries),
-            _ => to_std(&res_entries)
+            _ => to_std(&res_entries),
         }
     } else {
         let res_json = get_response2(&args, client);
-        let res_entries: Vec<UrlhausEntry> = res_json.urls.into_iter()
-            .filter(|e|
-                ((e.url_status.eq("online") && !args.exclude_online) ||
-                (e.url_status.eq("offline") && !args.exclude_offline)) &&
-                e.reporter.contains(&args.reporter) &&
-                e.dateadded >= Utc.datetime_from_str(&format!("{}{}", &args.date_from, "000000"), "%Y%m%d%H%M%S").unwrap_or(Utc::now()) &&
-                e.dateadded <= Utc.datetime_from_str(&format!("{}{}", &args.date_to, "000000"), "%Y%m%d%H%M%S").unwrap_or(Utc::now()))
+        let res_entries: Vec<UrlhausEntry> = res_json
+            .urls
+            .into_iter()
+            .filter(|e| {
+                ((e.url_status.eq("online") && !args.exclude_online)
+                    || (e.url_status.eq("offline") && !args.exclude_offline))
+                    && e.reporter.contains(&args.reporter)
+                    && e.dateadded
+                        >= Utc
+                            .datetime_from_str(
+                                &format!("{}{}", &args.date_from, "000000"),
+                                "%Y%m%d%H%M%S",
+                            )
+                            .unwrap_or(Utc::now())
+                    && e.dateadded
+                        <= Utc
+                            .datetime_from_str(
+                                &format!("{}{}", &args.date_to, "000000"),
+                                "%Y%m%d%H%M%S",
+                            )
+                            .unwrap_or(Utc::now())
+            })
             .collect();
         match args.format {
             Some(x) if x.to_lowercase().eq("csv") => {
                 let f = File::create("result.csv").expect("Unable to create file.");
-                let mut wtr = WriterBuilder::new().quote_style(QuoteStyle::Always).from_writer(BufWriter::new(f));
-                let _ = wtr.write_record(&["url_id", "url", "url_status", "dateadded", "reporter", "threat", "tags"]);
+                let mut wtr = WriterBuilder::new()
+                    .quote_style(QuoteStyle::Always)
+                    .from_writer(BufWriter::new(f));
+                let _ = wtr.write_record(&[
+                    "url_id",
+                    "url",
+                    "url_status",
+                    "dateadded",
+                    "reporter",
+                    "threat",
+                    "tags",
+                ]);
                 for e in res_entries {
-                    let _ = wtr.write_record(&[&e.url_id, &e.url.replace("http", "hxxp"), &e.url_status, &e.dateadded.to_string(), &e.reporter, &e.threat, &e.tags.join(":")]);
+                    let _ = wtr.write_record(&[
+                        &e.url_id,
+                        &e.url.replace("http", "hxxp"),
+                        &e.url_status,
+                        &e.dateadded.to_string(),
+                        &e.reporter,
+                        &e.threat,
+                        &e.tags.join(":"),
+                    ]);
                 }
                 println!("outputted. [{}].", "result.csv");
-            },
+            }
             Some(x) if x.to_lowercase().eq("json") => to_json(&res_entries),
-            _ => to_std(&res_entries)
+            _ => to_std(&res_entries),
         };
     }
 }
-
-
-
-
